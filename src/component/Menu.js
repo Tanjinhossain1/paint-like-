00f5bdb88ec26305d940594c6d7179b7bc35f155
tanjinhossain1 <tanjinhossain2003@gmail.com>
2022-07-14 12:57:58 +0600

    painting is done

---
+++ src/component/Menu.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Menu = ({ setLineColor, setLineWidth, 
+    setLineOpacity,download,clear }) => {
+    
+    return (
+        <div className="Menu">
+          <label>Brush Color </label>
+          <input
+            type="color"
+            onChange={(e) => {
+              setLineColor(e.target.value);
+            }}
+          />
+          <label>Brush Width </label>
+          <input
+            type="range"
+            min="3"
+            max="20"
+            onChange={(e) => {
+              setLineWidth(e.target.value);
+            }}
+          />
+          <label>Brush Opacity</label>
+          <input
+            type="range"
+            min="1"
+            max="100"
+            onChange={(e) => {
+              setLineOpacity(e.target.value / 100);
+            }}
+          />
+           <button className='download' onClick={download}>Download</button>
+           <button className='clear' onClick={clear}>Clear</button>
+        </div>
+      );
+};
+
+export default Menu;
---
+++ src/component/paint.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useRef, useState } from 'react';
+import Menu from './Menu';
+
+const Paint = () => {
+    const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [lineWidth, setLineWidth] = useState(5);
+  const [lineColor, setLineColor] = useState("black");
+  const [lineOpacity, setLineOpacity] = useState(0.1);
+  
+  // Initialization when the component
+  // mounts for the first time
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.globalAlpha = lineOpacity;
+    ctx.strokeStyle = lineColor;
+    ctx.lineWidth = lineWidth;
+    ctxRef.current = ctx;
+  }, [lineColor, lineOpacity, lineWidth]);
+  // download 
+  const download = async () => {
+    const image = canvasRef.current.toDataURL('image/png');
+    const blob = await (await fetch(image)).blob();
+    const blobURL = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = blobURL;
+    link.download = "image.png";
+    link.click();
+  }
+  // Function for starting the drawing
+  const startDrawing = (e) => {
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(
+      e.nativeEvent.offsetX, 
+      e.nativeEvent.offsetY
+    );
+    setIsDrawing(true);
+  };
+  // clear paint 
+  const clear = () => {
+    ctxRef.current.clearRect(0, 0, ctxRef.current.canvas.width, ctxRef.current.canvas.height)
+  }
+  // Function for ending the drawing
+  const endDrawing = () => {
+    ctxRef.current.closePath();
+    setIsDrawing(false);
+  };
+  
+  const draw = (e) => {
+    if (!isDrawing) {
+      return;
+    }
+    ctxRef.current.lineTo(
+      e.nativeEvent.offsetX, 
+      e.nativeEvent.offsetY
+    );
+      
+    ctxRef.current.stroke();
+  };
+    return (
+        <div>
+             <div className="App">
+      <h1>Paint App</h1>
+      <div className="draw-area">
+        <Menu
+          setLineColor={setLineColor}
+          setLineWidth={setLineWidth}
+          setLineOpacity={setLineOpacity}
+          download={download}
+          clear={clear}
+        />
+        <canvas
+          onMouseDown={startDrawing}
+          onMouseUp={endDrawing}
+          onMouseMove={draw}
+          ref={canvasRef}
+          width={`1280px`}
+          height={`720px`}
+        />
+     
+      </div>
+    </div>
+        </div>
+    );
+};
+
+export default Paint;
